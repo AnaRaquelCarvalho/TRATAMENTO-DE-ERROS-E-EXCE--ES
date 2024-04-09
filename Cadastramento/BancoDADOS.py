420e11e0da54cb9b3e3a1c25d6964a11b77581f8
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-04-09 14:55:56 -0300

    Cadastro de Pessoas

---
+++ Cadastramento/BancoDADOS.py
@@ -0,0 +1,23 @@
+def ArquivoExiste(nome):
+    try:
+        arquivo = open(nome,'LerArq')   #AbrirArquivo
+        arquivo.close()  #FecharArquivo
+    except FileNotFoundError:
+        return False
+    except:
+        return True
+
+def CriarArquivo(nome):
+    try:
+        arquivo = open(nome, 'criar +')
+        arquivo.close()
+    except:
+        print('Houve um ERRO na criação do arquivo')
+    else:
+        print('Arquivo {nome} criado com Sucesso!')        
+
+
+
+
+
+
