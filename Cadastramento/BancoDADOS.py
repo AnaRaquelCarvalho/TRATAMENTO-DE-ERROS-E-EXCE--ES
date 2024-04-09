0b76a4126ce6dcbe185da561e5ab572bdbd53195
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-04-09 18:44:16 -0300

    c

--- Cadastramento/BancoDADOS.py
+++ Cadastramento/BancoDADOS.py
@@ -1,6 +1,8 @@
+import Cadastramento.pessoas
+
 def ArquivoExiste(nome):
     try:
-        arquivo = open(nome,'LerArq')   #AbrirArquivo
+        arquivo = open(nome,'rt')   #AbrirArquivo
         arquivo.close()  #FecharArquivo
     except FileNotFoundError:
         return False
@@ -9,12 +11,40 @@ def ArquivoExiste(nome):
 
 def CriarArquivo(nome):
     try:
-        arquivo = open(nome, 'criar +')
+        arquivo = open(nome, 'wt+')
         arquivo.close()
     except:
         print('Houve um ERRO na criação do arquivo')
     else:
-        print('Arquivo {nome} criado com Sucesso!')        
+        print('Arquivo {nome} criado com Sucesso!')  
+
+def LerArquivo(nome):
+    try:
+        arquivo = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo!')
+    else:
+        Cadastramento.pessoas.cabeçalho('PESSOAS CADASTRADAS')
+        for linha in arquivo:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', ' ')
+            print(f'{dado[0]:<30} {dado[1]:>3} anos')
+    finally:
+        arquivo.close()            
+
+def cadastrar(arquivo, nome = 'desconhecido', idade=0):
+    try:
+        arquivo = open (arquivo, 'at')
+    except:
+        print('Houve um ERRO na abertura do arquivo')
+    else:
+        try:
+            arquivo.write(f'{nome}; {idade}\n')
+        except:
+            print(f'Novo registro de {nome} adicionado')
+            arquivo.close()
+
+
 
 
 
--- Cadastro.py
+++ Cadastro.py
@@ -1,7 +1,6 @@
 print('-='*20)
 print('CADASTRO DE PESSOAS'.center(38))
 
-
 import Cadastramento.pessoas
 import Cadastramento.BancoDADOS 
 from time import sleep
@@ -17,11 +16,18 @@ else:
 while True:
     resposta = Cadastramento.pessoas.menu(['VER PESSOAS CADASTRADAS', 'CADASTRAR NOVA PESSOA', 'SAIR DO SISTEMA'])
     if resposta == 1:
-        Cadastramento.pessoas.cabeçalho(' Opção 1')
+        #Opção de listar o conteúdo de um arquivo!
+        Cadastramento.BancoDADOS.LerArquivo(arquivo)
     elif resposta == 2:
-        Cadastramento.pessoas.cabeçalho(' Oção 2')
+        #Opção cadastrar novas pessoas
+        Cadastramento.pessoas.cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = Cadastramento.pessoas.LeiaInt('Idade: ')
+        Cadastramento.BancoDADOS.cadastrar(arquivo, nome, idade)
     elif resposta == 3:
+        #oPÇÃO DE SAIR DO SISTEMA
         Cadastramento.pessoas.cabeçalho(' SAINDO DO PROGRAMA...')          
         sleep(2)
+        break
     else:
         print('\033[31mERRO! Digite uma opção válida!\033[m')    
